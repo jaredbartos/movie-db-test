6b89dfd03231ec320dd16a8c0283e97ef4eaa409
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2023-10-09 18:44:57 -0500

    Add detailed section for clicked result, animation, and icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,34 +13,69 @@ $(document).ready(function() {
           $(this).children("p.resultType").text(data.Search[i].Type);
           if (data.Search[i].Poster !== "N/A") {
             $(this).children("img").attr("src", data.Search[i].Poster);
-          }
+          } else {
+            $(this).children("img").attr("src", "");
+          };
           $(this).attr("data-imdb-id", data.Search[i].imdbID);
-        }
-      })
+        };
+      });
+    };
+  };
+
+  var populateDetails = function(data) {
+    $("#searchResults").addClass("is-hidden");
+    $("#itemDetails").removeClass("is-hidden");
+    $("#detailsTitle").text(data.Title);
+    $("#detailsRated").text(data.Rated);
+    $("#detailsReleaseDate").text(data.Released);
+    $("#detailsRuntime").text(data.Runtime);
+    $("#detailsGenre").text(data.Genre);
+    $("#detailsDirector").text(data.Director);
+    $("#detailsWriter").text(data.Writer);
+    $("#detailsActors").text(data.Actors);
+    $("#detailsPlot").text(data.Plot);
+    $("#detailsAwards").text(data.Awards);
+    $("#detailsBoxOffice").text(data.BoxOffice);
+    if (data.Poster !== "N/A") {
+      $("#detailsPoster").attr("src", data.Poster);
     }
-  }
+  };
 
   var searchAPI = function(searchInput) {
-    var searchURL = "https://www.omdbapi.com/?apikey=" + omdbAPIKey + "&s=" + searchInput
+    var searchURL = "https://www.omdbapi.com/?apikey=" + omdbAPIKey + "&s=" + searchInput;
 
     fetch(searchURL)
       .then(function(response) {
         return response.json();
       })
       .then(function(data) {
-        console.log(data);
         populateSearchResults(data);
       })
-  }
+  };
 
-  var showEntryDetails = function() {
+  var fetchDetails = function(id) {
+    var titleURL = "https://www.omdbapi.com/?apikey=" + omdbAPIKey + "&plot=full&i=" + id;
 
-  }
+    fetch(titleURL)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        populateDetails(data);
+      })
+  };
 
   searchForm.on("submit", function(event) {
     event.preventDefault();
     searchAPI(searchBar.val());
     searchBar.val("");
-  })
+    $("#searchResults").addClass("is-hidden");
+    $("#itemDetails").addClass("is-hidden");
+  });
 
-})
+  $(".resultTitle").on("click", function(event) {
+    event.preventDefault();
+    fetchDetails($(this).parent().attr("data-imdb-id"))
+  });
+
+});
